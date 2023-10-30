3811f8f44e32e36c7fe5b333c13230bf6c9685d5
Rishi Kumar <rishi.kumar.2837@gmail.com>
2023-10-30 18:01:21 -0400

    Added CoroutineScope receiver to FunctionalState method.
    Each state can launch "global" state machine scoped coroutines.

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/statemachine/FunctionalState.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/statemachine/FunctionalState.kt
@@ -1,18 +1,21 @@
 package org.firstinspires.ftc.teamcode.statemachine
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.supervisorScope
+
 fun interface FunctionalState {
 
-    suspend fun runState(): FunctionalState
+    suspend fun CoroutineScope.runState(): FunctionalState
 
     object StopState : FunctionalState {
-        override suspend fun runState() = this
+        override suspend fun CoroutineScope.runState() = this@StopState
     }
 }
 
-suspend fun runStateMachine(state: FunctionalState) {
+suspend fun runStateMachine(state: FunctionalState) = supervisorScope {
     var currentState = state
 
     while (currentState != FunctionalState.StopState) {
-        currentState = currentState.runState()
+        currentState = with(currentState) { runState() }
     }
 }
