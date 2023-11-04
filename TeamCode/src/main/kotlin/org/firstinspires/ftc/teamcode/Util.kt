a6aeb83f01f81a82aeb885860add0db5221e2b32
FTC 7244 <ootb@ftc7244.org>
2023-11-04 14:53:58 -0400

    Added risingEdgeMonitor and fallingEdgeMonitor property delegates

---
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/Util.kt
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode
+
+import com.outoftheboxrobotics.suspendftc.loopYieldWhile
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.withIndex
+import kotlinx.coroutines.launch
+import kotlin.properties.ReadOnlyProperty
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+interface ReadOnlyProperty<out V> : ReadOnlyProperty<Any?, V> {
+    val value: V
+
+    override fun getValue(thisRef: Any?, property: KProperty<*>) = value
+}
+
+interface ReadWriteProperty<V> : ReadWriteProperty<Any?, V> {
+    var value: V
+
+    override fun getValue(thisRef: Any?, property: KProperty<*>) = value
+    override fun setValue(thisRef: Any?, property: KProperty<*>, value: V) { this.value = value }
+}
+
+suspend fun <T> StateFlow<T>.next() = withIndex().first { it.index > 0 }.value
+
+fun CoroutineScope.risingEdgeMonitor(observed: () -> Boolean): ReadOnlyProperty<Any?, Boolean> {
+    var last = observed()
+    var current = observed()
+
+    launch {
+        loopYieldWhile({ true }) {
+            last = current
+            current = observed()
+        }
+    }
+
+    return ReadOnlyProperty { _, _ -> current && !last}
+}
+
+fun CoroutineScope.fallingEdgeMonitor(observed: () -> Boolean): ReadOnlyProperty<Any?, Boolean> {
+    var last = observed()
+    var current = observed()
+
+    launch {
+        loopYieldWhile({ true }) {
+            last = current
+            current = observed()
+        }
+    }
+
+    return ReadOnlyProperty { _, _ -> !current && last}
+}
