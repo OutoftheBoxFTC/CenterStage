a67593495678463504469dd6f64d7bf4cde80d33
FTC 7244 <ootb@ftc7244.org>
2023-11-18 15:03:43 -0500

    Added current monitoring to ExtensionTest

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opmodes/testing/ExtensionTest.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opmodes/testing/ExtensionTest.kt
@@ -2,7 +2,11 @@ package org.firstinspires.ftc.teamcode.opmodes.testing
 
 import arrow.core.some
 import com.outoftheboxrobotics.suspendftc.loopYieldWhile
+import com.outoftheboxrobotics.suspendftc.suspendFor
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit
 import org.firstinspires.ftc.teamcode.hardware.devices.ThreadedImuHandler
 import org.firstinspires.ftc.teamcode.opmodes.RobotOpMode
 import org.firstinspires.ftc.teamcode.util.G
@@ -16,11 +20,23 @@ class ExtensionTest : RobotOpMode(
     override suspend fun runSuspendOpMode() {
         suspendUntilStart()
 
-        loopYieldWhile({ true }) {
-            G.ehub.setExtensionPower(-gamepad1.left_stick_y.toDouble())
+        coroutineScope {
+            launch {
+                loopYieldWhile({ true }) {
+                    suspendFor(50)
+                    G.ehub.readCurrents()
+                }
+            }
 
-            telemetry["encoder pos"] = G.ehub.m7.currentPosition
-            telemetry["encoder vel"] = G.ehub.m7.velocity
+            loopYieldWhile({ true }) {
+                G.ehub.setExtensionPower(-gamepad1.left_stick_y.toDouble())
+
+                telemetry["encoder pos"] = G.ehub.m7.currentPosition
+                telemetry["encoder vel"] = G.ehub.m7.velocity
+                telemetry["current"] = G.ehub.run {
+                    leftExtension.getCurrent(CurrentUnit.AMPS) + rightExtension.getCurrent(CurrentUnit.AMPS)
+                }
+            }
         }
     }
 }
