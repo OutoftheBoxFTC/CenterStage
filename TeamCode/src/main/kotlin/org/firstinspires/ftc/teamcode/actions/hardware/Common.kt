6682d32749227dfc9fc3d560cc923d743c6e5103
Rishi Kumar <rishi.kumar.2837@gmail.com>
2023-12-06 21:45:31 -0500

    Added resetDrivePose that handles drivetrain pose and imu angle reset

---
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/actions/hardware/Common.kt
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.actions.hardware
+
+import com.acmerobotics.roadrunner.geometry.Pose2d
+import org.firstinspires.ftc.teamcode.RobotState
+import org.firstinspires.ftc.teamcode.driveState
+import org.firstinspires.ftc.teamcode.util.G
+
+fun resetDrivePose(newPose: Pose2d) {
+    resetImuAngle(newPose.heading)
+    G[RobotState.driveState.currentPose] = newPose
+}
