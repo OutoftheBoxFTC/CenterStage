6a1234ef0ed061c84c54e4176eb33e6e1632cfa2
Rishi Kumar <rishi.kumar.2837@gmail.com>
2023-12-28 11:13:07 -0500

    Retuned tilt positions

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/actions/hardware/Intake.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/actions/hardware/Intake.kt
@@ -3,14 +3,14 @@ package org.firstinspires.ftc.teamcode.actions.hardware
 import org.firstinspires.ftc.teamcode.util.G
 
 enum class IntakeTiltPosition(val pos: Double) {
-    HIGH(0.144),
+    HIGH(0.688),
 
-    POS_1(0.238),
-    POS_2(0.286),
-    POS_3(0.347),
-    POS_4(0.406),
+    POS_1(0.756),
+    POS_2(0.816),
+    POS_3(0.861),
+    POS_4(0.913),
 
-    LOW(0.446)
+    LOW(0.969)
 }
 
 fun setTiltPosition(pos: Double) { G.ehub.intakeTilt.position = pos }
