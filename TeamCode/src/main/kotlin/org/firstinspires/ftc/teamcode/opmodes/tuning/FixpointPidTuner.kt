28c37948d6d867a436e6d1304a64ddfdfe965f2c
Rishi Kumar <rishi.kumar.2837@gmail.com>
2024-01-02 12:34:51 -0500

    Tuned PID Fixpoint with kStatic Added better path follower

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opmodes/tuning/FixpointPidTuner.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opmodes/tuning/FixpointPidTuner.kt
@@ -1,44 +1,52 @@
 package org.firstinspires.ftc.teamcode.opmodes.tuning
 
+import arrow.core.merge
 import arrow.core.nel
+import arrow.fx.coroutines.raceN
 import com.acmerobotics.dashboard.config.Config
 import com.acmerobotics.roadrunner.geometry.Pose2d
 import com.outoftheboxrobotics.suspendftc.loopYieldWhile
 import com.outoftheboxrobotics.suspendftc.suspendUntil
-import com.qualcomm.robotcore.eventloop.opmode.Disabled
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
+import org.firstinspires.ftc.teamcode.RobotState
+import org.firstinspires.ftc.teamcode.Subsystem
 import org.firstinspires.ftc.teamcode.actions.controllers.PidCoefs
 import org.firstinspires.ftc.teamcode.actions.controllers.runPosePidController
+import org.firstinspires.ftc.teamcode.actions.hardware.DriveConfig
 import org.firstinspires.ftc.teamcode.actions.hardware.currentDrivePose
+import org.firstinspires.ftc.teamcode.actions.hardware.launchFixpoint
 import org.firstinspires.ftc.teamcode.actions.hardware.setAdjustedDrivePowers
 import org.firstinspires.ftc.teamcode.actions.hardware.setDrivePowers
-import org.firstinspires.ftc.teamcode.Subsystem
+import org.firstinspires.ftc.teamcode.driveLooper
 import org.firstinspires.ftc.teamcode.opmodes.RobotOpMode
 import org.firstinspires.ftc.teamcode.runStateMachine
 import org.firstinspires.ftc.teamcode.util.C
 import org.firstinspires.ftc.teamcode.util.FS
 import org.firstinspires.ftc.teamcode.util.G
 import org.firstinspires.ftc.teamcode.util.launchCommand
+import org.firstinspires.ftc.teamcode.util.mainLoop
 import org.firstinspires.ftc.teamcode.util.set
 
 @TeleOp
 @Config
-@Disabled
 class FixpointPidTuner : RobotOpMode() {
     companion object {
         @JvmField var target_x = 0.0
         @JvmField var target_y = 0.0
         @JvmField var target_heading = 0.0
 
-        @JvmField var heading_kP = 3.2
-        @JvmField var heading_kI = 0.0
-        @JvmField var heading_kD = 0.2
+        @JvmField var heading_kP = DriveConfig.headingPid.kP
+        @JvmField var heading_kI = DriveConfig.headingPid.kI
+        @JvmField var heading_kD = DriveConfig.headingPid.kD
 
-        @JvmField var translational_kP = 0.3
-        @JvmField var translational_kI = 0.0
-        @JvmField var translational_kD = 0.02
+        @JvmField var translational_kP = DriveConfig.translationalPid.kP
+        @JvmField var translational_kI = DriveConfig.translationalPid.kI
+        @JvmField var translational_kD = DriveConfig.translationalPid.kD
+
+        @JvmField var kStaticOffset = 0.05
     }
 
     private val translationalCoefs = PidCoefs(0.0, 0.0, 0.0)
@@ -58,18 +66,34 @@ class FixpointPidTuner : RobotOpMode() {
         }
     }
 
-    private val fixpointState: FS = FS {
+    private val kStaticState: FS = FS {
         G.cmd.launchCommand(Subsystem.DRIVETRAIN.nel()) {
-            runPosePidController(
-                translationalCoefs = translationalCoefs,
-                headingCoefs = headingCoefs,
-                input = ::currentDrivePose,
-                target = { Pose2d(target_x, target_y, target_heading) },
-                output = { setDrivePowers(it.x, it.y, it.heading) }
-            )
+            mainLoop {
+                setDrivePowers(0.0, 0.0, kStaticOffset)
+            }
         }
 
         loopYieldWhile({ !gamepad1.y }) {
+            telemetry["Current State"] = "kStatic tuning"
+        }
+
+        fixpointState
+    }
+
+    private val fixpointState: FS = FS {
+        G[RobotState.driveLooper].scheduleCoroutine {
+            G.cmd.runNewCommand(Subsystem.DRIVETRAIN.nel()) {
+                runPosePidController(
+                    translationalCoefs = translationalCoefs,
+                    headingCoefs = headingCoefs,
+                    input = ::currentDrivePose,
+                    target = { Pose2d(target_x, target_y, target_heading) },
+                    output = { setAdjustedDrivePowers(it.x, it.y, it.heading) }
+                )
+            }
+        }
+
+        loopYieldWhile({ !gamepad1.b }) {
             telemetry["Current State"] = "PID Fixpoint"
         }
 
@@ -88,8 +112,28 @@ class FixpointPidTuner : RobotOpMode() {
             }
         }
 
-        suspendUntil { gamepad1.x }
-        fixpointState
+        raceN(
+            coroutineContext,
+            {
+                suspendUntil { gamepad1.x }
+                kStaticState
+            },
+            {
+                suspendUntil { gamepad1.a }
+                standardFixpointState
+            }
+        ).merge()
+    }
+
+    private val standardFixpointState: FS = FS {
+        val job = launchFixpoint(Pose2d(target_x, target_y, target_heading))
+
+        loopYieldWhile({ !gamepad1.b }) {
+            telemetry["Current State"] = "Standard Fixpoint"
+        }
+
+        job.cancelAndJoin()
+        freeControlState
     }
 
     override suspend fun runSuspendOpMode() {
