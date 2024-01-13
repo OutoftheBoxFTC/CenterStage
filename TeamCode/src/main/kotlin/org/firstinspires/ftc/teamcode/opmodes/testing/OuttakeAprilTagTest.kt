f2d57732f7e0511031a0109516ebfcef49d92fe4
Rishi Kumar <rishi.kumar.2837@gmail.com>
2024-01-13 11:14:38 -0500

    Added backstage apriltag position detection

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opmodes/testing/OuttakeAprilTagTest.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opmodes/testing/OuttakeAprilTagTest.kt
@@ -1,23 +1,35 @@
 package org.firstinspires.ftc.teamcode.opmodes.testing
 
+import arrow.fx.coroutines.raceN
+import com.acmerobotics.roadrunner.geometry.Pose2d
+import com.outoftheboxrobotics.suspendftc.loopYieldWhile
+import com.outoftheboxrobotics.suspendftc.suspendUntil
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
-import com.qualcomm.robotcore.eventloop.opmode.Disabled
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation
 import org.firstinspires.ftc.teamcode.RobotState
+import org.firstinspires.ftc.teamcode.actions.hardware.currentDrivePose
+import org.firstinspires.ftc.teamcode.actions.hardware.launchOuttakeFixpoint
+import org.firstinspires.ftc.teamcode.actions.hardware.nextBackboardApriltagPosition
 import org.firstinspires.ftc.teamcode.opmodes.RobotOpMode
 import org.firstinspires.ftc.teamcode.util.G
 import org.firstinspires.ftc.teamcode.util.mainLoop
+import org.firstinspires.ftc.teamcode.util.merge
+import org.firstinspires.ftc.teamcode.util.set
 import org.firstinspires.ftc.teamcode.vision.AprilTagDetectionPipeline
+import org.firstinspires.ftc.teamcode.vision.PreloadDetectionPipeline
 import org.firstinspires.ftc.teamcode.vision.aprilTagDetections
 import org.firstinspires.ftc.teamcode.visionState
 import org.openftc.apriltag.AprilTagDetection
 import org.openftc.easyopencv.OpenCvCameraRotation
 
 @Autonomous
-@Disabled
 class OuttakeAprilTagTest : RobotOpMode() {
     override suspend fun runSuspendOpMode() {
         G.chub.outtakeCamera.let {
@@ -26,13 +38,51 @@ class OuttakeAprilTagTest : RobotOpMode() {
             it.setPipeline(AprilTagDetectionPipeline.outtakePipeline())
         }
 
-        mainLoop {
-            val detections = G[RobotState.visionState.aprilTagDetections]
+        var backboardPose: Pose2d? = Pose2d()
+        val targetPos: Deferred<PreloadDetectionPipeline.RandomizationPosition>
 
-            detections.forEach {
-                telemetry.addLine(it.formatted())
+        coroutineScope {
+            targetPos = async {
+                raceN(
+                    coroutineContext,
+                    {
+                        suspendUntil { gamepad1.x }
+                        PreloadDetectionPipeline.RandomizationPosition.LEFT
+                    },
+                    {
+                        suspendUntil { gamepad1.y }
+                        PreloadDetectionPipeline.RandomizationPosition.CENTER
+                    },
+                    {
+                        suspendUntil { gamepad1.b }
+                        PreloadDetectionPipeline.RandomizationPosition.RIGHT
+                    }
+                ).merge()
+            }
+
+            launch {
+                while (!targetPos.isCompleted) {
+                    backboardPose = nextBackboardApriltagPosition()
+                }
+            }
+
+            loopYieldWhile({ !targetPos.isCompleted }) {
+                val detections = G[RobotState.visionState.aprilTagDetections]
+
+                detections.forEach {
+                    telemetry.addLine(it.formatted())
+                }
+
+                backboardPose?.let {
+                    telemetry["outtake"] = it
+                    telemetry["delta"] = it - currentDrivePose()
+                }
             }
         }
+
+        launchOuttakeFixpoint(Pose2d(), targetPos.await())
+
+        mainLoop {  }
     }
 
     private fun AprilTagDetection.formatted(): String {
