103aa0c192498e44a5bbbef94f2ff35c559dd209
Rishi Kumar <rishi.kumar.2837@gmail.com>
2024-02-26 09:06:41 -0500

    Added nicer pose telemetry to Bozo opmode Added manual intake power to IntakeTest

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opmodes/Bozo.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opmodes/Bozo.kt
@@ -2,7 +2,13 @@ package org.firstinspires.ftc.teamcode.opmodes
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import org.firstinspires.ftc.teamcode.LoggingConfig
+import org.firstinspires.ftc.teamcode.RobotState
+import org.firstinspires.ftc.teamcode.actions.hardware.currentDrivePose
+import org.firstinspires.ftc.teamcode.actions.hardware.extendoPose
+import org.firstinspires.ftc.teamcode.driveState
+import org.firstinspires.ftc.teamcode.util.G
 import org.firstinspires.ftc.teamcode.util.mainLoop
+import org.firstinspires.ftc.teamcode.util.set
 
 @Autonomous
 class Bozo : RobotOpMode() {
@@ -11,7 +17,13 @@ class Bozo : RobotOpMode() {
 
         try {
             LoggingConfig.queryString = "*"
-            mainLoop {  }
+            mainLoop {
+                val cp = currentDrivePose()
+                val ep = G[RobotState.driveState.extendoPose]
+
+                telemetry["Drive Pose"] = "Pose2d(${cp.x}, ${cp.y}, ${cp.heading})"
+                telemetry["Intake Pose"] = "Pose2d(${ep.x}, ${ep.y}, ${ep.heading})"
+            }
         } finally {
             LoggingConfig.queryString = oldLogState
         }
